3f4db0c963b02140f49c3ae01317dd627061e274
sangramrath <sangram.rath@gmail.com>
2022-10-23 11:59:36 +0530

    Update install-on-vm.sh

--- scripts/install-on-vm.sh
+++ scripts/install-on-vm.sh
@@ -16,14 +16,12 @@ mysql -u root -e "create user strapi@localhost identified by 'mysecurepassword';
 mysql -u root -e "grant all privileges on strapi_dev.* to strapi@localhost;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 chown -R strapi:strapi /srv/strapi
-sudo su - strapi
+sudo su strapi
 cd /srv/strapi/
 yarn create strapi-app blog --quickstart --no-run
 cd /srv/strapi/blog/
 rm -rf config/database.js
-cat << EOF > config/database.js 
-module.exports = { apps: [ { name: 'strapi-dev', cwd: '/srv/strapi/blog', script: 'npm', args: 'start', env: { NODE_ENV: 'development', DB_HOST: 'localhost', DB_PORT: '3306', DB_NAME: 'strapi_dev', DB_USER: 'strapi', DB_PASS: 'mysecurepassword', JWT_SECRET: 'aSecretKey', }, }, ], }; 
-EOF
+wget https://raw.githubusercontent.com/sangramrath/strapi.io/main/scripts/mysql/database.js -P config/
 npm install mysql
 npm run build
 yarn global add pm2
@@ -36,5 +34,5 @@ pm2 start ecosystem.config.js
 pm2 startup systemd
 exit
 env PATH=$PATH:/usr/bin /home/strapi/.config/yarn/global/node_modules/pm2/bin/pm2 startup systemd -u strapi --hp /home/strapi
-sudo su - strapi
+sudo su strapi
 pm2 save
